ef0f2d76289181153db9d2541b3f849755790693
ramen0702 <zettai72masa@gmail.com>
2022-08-15 16:41:28 +0900

    add login

--- secret_chat/urls.py
+++ secret_chat/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.contrib.auth.decorators import login_required
+
+from django.views.generic import TemplateView
+index_view = TemplateView.as_view(template_name="website/index.html")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('website.urls')),
+    path("", login_required(index_view), name="index"),
+    path('', include("django.contrib.auth.urls")),
 ]
